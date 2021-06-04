cc7d0fc0f5774814328f3ec497939eff83c97f5c
mhautofilterv5 <85279476+mhautofilterv5@users.noreply.github.com>
2021-06-04 23:25:27 +0530

    Update __main__.py

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -20,7 +20,7 @@ from tg_bot.modules.helper_funcs.misc import paginate_modules
 PM_START_TEXT = """
 hoi {}, my name is {}! if you have any questions about how to use me please give me /help... 
 
-im a group manager bot maintained by  [this person](tg://user?id={}).
+im a group manager bot maintained by  [this person](tg:/malluhubbmovies/user?id={}).
 
 My future updates will be put into This Channel 🎬 - @MALLUZHUB & My Support Group 🚧 @malluhubbmovies.
 
