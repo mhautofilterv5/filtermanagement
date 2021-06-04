b53419a5209d9fc29cdfa7620ca908d0e59e3cb0
mhautofilterv5 <85279476+mhautofilterv5@users.noreply.github.com>
2021-06-04 23:24:09 +0530

    Update __main__.py

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -22,9 +22,8 @@ hoi {}, my name is {}! if you have any questions about how to use me please give
 
 im a group manager bot maintained by  [this person](tg://user?id={}).
 
-My future updates will be put into This Channel - @MarieChechi & My Support Group @InFoTelGroup.
+My future updates will be put into This Channel 🎬 - @MALLUZHUB & My Support Group 🚧 @malluhubbmovies.
 
-This is my [Deploy Code](https://heroku.com/deploy?template=https://github.com/TGExplore/Marie-2.0-English),
 you can create clone same like me..
 
 For more commands click /help...
@@ -50,10 +49,10 @@ And the following:
 """.format(dispatcher.bot.first_name, "" if not ALLOW_EXCL else "\nAll of the following commands  / or ! can  be used...\n")
 
 DONATE_STRING = """Heya, glad to hear you want to donate!
-It took lots of work for [my creator](t.me/SonOfLars) to get me to where I am now, and every donation helps \
-motivate him to make me even better. All the donation money will go to a better VPS to host me, and/or beer \
+It took lots of work for [my creator](t.me/malluhubbmovies) to get me to where I am now, helps \
+motivate him to make me even better. i will go to a better VPS to host me, and/or beer \
 (see his bio!). He's just a poor student, so every little helps!
-There are two ways of paying him; [PayPal](paypal.me/PaulSonOfLars), or [Monzo](monzo.me/paulnionvestergaardlarsen)."""
+"""
 
 IMPORTED = {}
 MIGRATEABLE = []
